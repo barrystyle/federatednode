94f3a41e2bea53af3c4765e85cb64a542e395bb9
Matt Smith <matt.joseph.smith@gmail.com>
2020-08-15 14:21:27 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+**All-in-one Aspire Federated Node Build System (`federatednode`)**
+
+Allows one to easily build a aspire-server, `aspireblock`, aspire wallet system and/or aspire-scout (an explorer), with all required components. Uses Docker and Docker compose.
+
+Please issue any/all pull requests against **develop**, not **master**.
---
+++ config/aspire-scout/mainnet.py
@@ -0,0 +1,55 @@
+from celery.schedules import crontab
+from datetime import timedelta
+from decimal import Decimal
+import logging
+
+DEBUG = True
+TESTING = False
+ASSETS_DEBUG = True
+CSRF_SESSION_KEY = "blahblahblah"
+SECRET_KEY = "blahblahblah"
+GEOCITY_DAT_LOCATION = "/scout/scout/libs/GeoLiteCity.dat"
+LOGGING_LEVEL = logging.DEBUG
+LOGGING_FILE = "/scout/app.log"
+PORT = 8182
+
+SENTRY_CONFIG = {
+    'dsn': '',
+    'environment': 'matts dev'
+}
+
+MONGODB_SETTINGS = {
+    'DB': 'scout-m',
+    'HOST': 'mongodb'
+}
+
+CELERYBEAT_SCHEDULE = {
+}
+
+MAX_CONTENT_LENGTH = 10 * 1024 * 1024  # 5MB
+
+# MAILGUN
+MAILGUN_DOMAIN = '.mailgun.org'
+MAILGUN_API_KEY = 'key-'
+MAILGUN_DEFAULT_FROM = 'Mailgun Sandbox <postmaster@.mailgun.org>'
+
+# REDIS
+REDIS_HOST = 'redis'
+REDIS_PORT = 6379
+REDIS_DB = 3
+
+# CELERY
+CELERY_TASK_SERIALIZER = 'custom_json'
+CELERY_ACCEPT_CONTENT = ['custom_json']
+CELERY_RESULT_BACKEND = "redis/3"
+CELERY_BROKER_URL = "redis/3"
+
+ASPIRE_BLOCK_URL = 'http://aspireblock:4100/api/'
+ASPIRE_BLOCK_USER = 'rpc'
+ASPIRE_BLOCK_PASS = 'rpc'
+
+ASPIRE_GAS_HOST = 'gasp'
+ASPIRE_GAS_PORT = 8332
+ASPIRE_GAS_USER = 'rpc'
+ASPIRE_GAS_PASS = 'rpc'
+
